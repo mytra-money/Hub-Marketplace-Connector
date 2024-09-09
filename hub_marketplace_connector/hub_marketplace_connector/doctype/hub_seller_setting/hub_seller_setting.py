9c30274e32d2c1513f8b2085a32a04e3c5d36282
Harsh Agrawal <ha@pwctech.in>
2024-09-09 15:48:06 +0530

    Added the basic required doctypes for Hub Marketplace Sellers

---
+++ hub_marketplace_connector/hub_marketplace_connector/doctype/hub_seller_setting/hub_seller_setting.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, pwctech technologies private limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class HubSellerSetting(Document):
+	pass
---
+++ hub_marketplace_connector/hub_marketplace_connector/doctype/hub_seller_warehouse_setting/test_hub_seller_warehouse_setting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, pwctech technologies private limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestHubSellerWarehouseSetting(FrappeTestCase):
+	pass
