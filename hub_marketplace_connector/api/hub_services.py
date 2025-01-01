7e81f6b612a171efa09f11fb817be646199fa1de
Harsh Agrawal <ha@pwctech.in>
2025-01-01 13:57:17 +0530

    Changed the URL and other fixes to connect with Hub Marketplace

--- hub_marketplace_connector/api/hub_services.py
+++ hub_marketplace_connector/api/hub_services.py
@@ -8,15 +8,12 @@ class HubServices:
         self.set_base_url()
     
     def set_base_url(self):
+        #TODO: change to live URL
+        self.base_url = "https://demo.hubmarket.place/api/method/hub_marketplace/"
         if frappe.db.exists("Hub Seller Setting"):
             hub_seller_settings = frappe.get_cached_doc("Hub Seller Setting")
             if hub_seller_settings.environment == "Pre-Production":
-                self.base_url = "https://sit.mytra.money/api/method/hub_marketplace/"
-            else:
-                #TODO: Change the URL to the new site created for Hub
-                self.base_url = "https://sit.mytra.money/api/method/hub_marketplace/"
-        else:
-            self.base_url = "https://sit.mytra.money/api/method/hub_marketplace/"
+                self.base_url = "https://demo.hubmarket.place/api/method/hub_marketplace/"
     
     def get_categories(self):
         url = self.base_url+"get_seller_categories"
@@ -29,16 +26,16 @@ class HubServices:
         return response.get("message")
     
     def register_user(self, user_data):
-        url = self.base_url+"register_user"
+        url = self.base_url+"register_seller"
         response = request(method="POST", url=url, data=frappe.as_json(user_data), headers={"Content-Type": "application/json"}).json()
         return response.get("message")
     
     def send_catalog(self, catalog):
         url = self.base_url+"update_catalog"
-        hub_seller_setting = frappe.get_doc("Hub Seller Setting")
+        hub_seller_setting = frappe.get_cached_doc("Hub Seller Setting")
         headers = {
             "Content-Type": "application/json",
-            "Authorization": "token {}:{}".format(hub_seller_setting.api_key, hub_seller_setting.get_password("api_secret"))
+            "Authorization": "token {0}:{1}".format(hub_seller_setting.api_key, hub_seller_setting.get_password("api_secret"))
         }
         integration_request = create_request_log(data=catalog, service_name="Hub Marketplace Catalog", request_headers=headers, url=url)
         response = request(method="POST", url=url, data=catalog, headers=headers)
